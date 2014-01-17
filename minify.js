d3c1bca849e6e85469e5121eed6c37d964c5db56
Dave Longley <dlongley@digitalbazaar.com>
2014-01-17 13:00:36 -0500

    Fix minify script.

--- minify.js
+++ minify.js
@@ -1,12 +1,12 @@
 ({
   baseUrl: 'site/static',
   paths: {
-    'underscore': '../../../node_modules/underscore',
+    'underscore': '../../node_modules/underscore',
     // override templates
     'app/templates': 'app/templates.min'
   },
   mainConfigFile: 'site/static/app/main.js',
-  name: '../../../node_modules/almond/almond',
+  name: '../../node_modules/almond/almond',
   include: ['app/main'],
   insertRequire: ['app/main'],
   out: 'site/static/app/main.min.js',
