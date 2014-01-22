88f45b97c5d035e2c664da29e693aeb298110ca1
David I. Lehn <dlehn@digitalbazaar.com>
2014-01-22 15:55:09 -0500

    Split config into task sized pieces.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,78 @@
+/*
+ * Bedrock Gruntfile.
+ *
+ * Copyright (c) 2013-2014 Digital Bazaar, Inc. All rights reserved.
+ */
 module.exports = function(grunt) {
   'use strict';
 
-  // project configuration
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    cssmin: {
-      combine: {
-        options: {
-          root: 'site/static/',
-          report: 'min'
+  // init config
+  grunt.initConfig({});
+
+  // read package configuration
+  grunt.config('pkg', grunt.file.readJSON('package.json'));
+
+  // cssmin
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.config('cssmin', {
+    combine: {
+      options: {
+        root: 'site/static/',
+        report: 'min'
+      },
+      files: {
+        'site/static/css/bundle.min.css': [
+          'site/static/bootstrap/css/bootstrap.css',
+          'site/static/bootstrap/css/bootstrap-responsive.css',
+          'site/static/font-awesome/css/font-awesome.css',
+          'site/static/css/common.css',
+          'site/static/css/custom.css'
+        ]
+      }
+    }
+  });
+
+  // angular templates
+  grunt.loadNpmTasks('grunt-angular-templates');
+  grunt.config('ngtemplates', {
+    myapp: {
+      options: {
+        // the module the templates will be added to
+        module: 'app.templates',
+        htmlmin: {
+          collapseBooleanAttributes:      true,
+          collapseWhitespace:             true,
+          removeAttributeQuotes:          true,
+          removeComments:                 true,
+          removeEmptyAttributes:          true,
+          removeRedundantAttributes:      true,
+          removeScriptTypeAttributes:     true,
+          removeStyleLinkTypeAttributes:  true
         },
-        files: {
-          'site/static/css/bundle.min.css': [
-            'site/static/bootstrap/css/bootstrap.css',
-            'site/static/bootstrap/css/bootstrap-responsive.css',
-            'site/static/font-awesome/css/font-awesome.css',
-            'site/static/css/common.css',
-            'site/static/css/custom.css'
-          ]
+        bootstrap: function(module, script) {
+          return [
+            "define(['angular'], function(angular) {\n",
+            "angular.module('" + module + "', [])",
+            ".run(['$templateCache', function($templateCache) {\n",
+            script,
+            '}]);\n});\n'].join('');
+        },
+        url: function(file) {
+          var idx = file.indexOf('site/static');
+          file = file.substr(idx + 'site/static'.length);
+          return file;
         }
-      }
-    },
-    ngtemplates: {
-      myapp: {
-        options: {
-          // the module the templates will be added to
-          module: 'app.templates',
-          htmlmin: {
-            collapseBooleanAttributes:      true,
-            collapseWhitespace:             true,
-            removeAttributeQuotes:          true,
-            removeComments:                 true,
-            removeEmptyAttributes:          true,
-            removeRedundantAttributes:      true,
-            removeScriptTypeAttributes:     true,
-            removeStyleLinkTypeAttributes:  true
-          },
-          bootstrap: function(module, script) {
-            return [
-              "define(['angular'], function(angular) {\n",
-              "angular.module('" + module + "', [])",
-              ".run(['$templateCache', function($templateCache) {\n",
-              script,
-              '}]);\n});\n'].join('');
-          },
-          url: function(file) {
-            var idx = file.indexOf('site/static');
-            file = file.substr(idx + 'site/static'.length);
-            return file;
-          }
-        },
-        src: 'site/static/app/templates/**/*.html',
-        dest: 'site/static/app/templates.min.js'
-      }
-    },
-    jshint: {
-      all: [
+      },
+      src: 'site/static/app/templates/**/*.html',
+      dest: 'site/static/app/templates.min.js'
+    }
+  });
+
+  // jshint
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.config('jshint', {
+    all: {
+      src: [
         '*.js',
         'bin/*.js',
         'configs/*.js',
@@ -72,11 +89,6 @@ module.exports = function(grunt) {
       ]
     }
   });
-
-  // plugins
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-angular-templates');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // default tasks
   grunt.registerTask('default', ['ngtemplates', 'cssmin']);
