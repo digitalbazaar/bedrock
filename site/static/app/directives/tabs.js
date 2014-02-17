96c17974c1d2db79799f0e6acb157b1c0dd37441
Dave Longley <dlongley@digitalbazaar.com>
2014-02-17 14:45:25 -0500

    Fix whitespace.

--- site/static/app/directives/tabs.js
+++ site/static/app/directives/tabs.js
@@ -18,16 +18,16 @@ return {
 function tabsFactory() {
   function Ctrl($scope) {
     var panes = $scope.panes = [];
- 
+
     $scope.select = function(pane) {
       angular.forEach(panes, function(pane) {
         pane.selected = false;
       });
       pane.selected = true;
     };
- 
+
     this.addPane = function(pane) {
-      if (panes.length === 0) {
+      if(panes.length === 0) {
         //$scope.select(pane);
         pane.selected = true;
       }
