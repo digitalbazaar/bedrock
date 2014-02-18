1a504201c249db0cd3092cef409fe29fc000e458
Dave Longley <dlongley@digitalbazaar.com>
2014-02-18 11:36:06 -0500

    Add private key pem schema.

---
+++ schemas/privateKeyPem.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2012-2014 Digital Bazaar, Inc. All rights reserved.
+ */
+var tools = require(__libdir + '/bedrock/tools');
+
+var schema = {
+  required: true,
+  title: 'Private Key PEM',
+  description: 'A cryptographic Private Key in PEM format.',
+  type: 'string',
+  pattern: '^\\s*-----BEGIN RSA PRIVATE KEY-----[a-zA-Z0-9/+=\\s]*-----END RSA PRIVATE KEY-----\\s*$',
+  errors: {
+    invalid: 'The private key is not formatted correctly.',
+    missing: 'The private key is missing.'
+  }
+};
+
+module.exports = function(extend) {
+  if(extend) {
+    return tools.extend(true, tools.clone(schema), extend);
+  }
+  return schema;
+};
