286c8161864e0ef39377d2164cd853241138489b
Dave Longley <dlongley@digitalbazaar.com>
2014-03-11 16:17:23 -0400

    Change how resource role resource properties are generated.

--- configs/common-data.js
+++ configs/common-data.js
@@ -3,15 +3,14 @@ var config = require(__libdir + '/bedrock').config;
 // identity config
 config.identity.defaults.identity = {
   type: 'Identity',
-  sysStatus: 'active',
-  sysPublic: [],
   address: [],
   preferences: {
     type: 'IdentityPreferences'
-  }
+  },
+  sysPublic: [],
+  sysResourceRole: [{
+    sysRole: 'identity.registered',
+    generateResource: 'id'
+  }],
+  sysStatus: 'active'
 };
-
-// default resource roles
-config.identity.defaults.roles = [{
-  sysRole: 'identity.registered'
-}];
--- configs/dev-data.js
+++ configs/dev-data.js
@@ -4,17 +4,17 @@ var baseUri = config.server.baseUri;
 // profiles
 config.profile.profiles.push({
   type: 'Profile',
-  sysSlug: 'dev',
-  email: 'dev@bedrock.dev',
-  label: 'Dev',
-  sysPassword: 'password',
-  sysRole: [baseUri + '/roles/profile_registered']
 });
 
 // identities
 config.identity.identities.push({
   type: 'Identity',
-  owner: baseUri + '/profiles/dev',
   sysSlug: 'dev',
-  label: 'Dev'
+  label: 'Dev',
+  email: 'dev@bedrock.dev',
+  sysPassword: 'password',
+  sysResourceRole: [{
+    sysRole: 'identity.registered',
+    generateResource: 'id'
+  }]
 });
