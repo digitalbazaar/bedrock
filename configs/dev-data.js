c99b63ec56c1694a57b82f50d314ec1a7e7cb84a
David I. Lehn <dlehn@digitalbazaar.com>
2014-04-18 16:01:41 -0400

    Add identity service.
    - Add .well-known/identity endpoint with identityService and owner data.
    - Add identity owner config value.
    - Add dev site 'bedrock' id as identity owner.

--- configs/dev-data.js
+++ configs/dev-data.js
@@ -3,6 +3,17 @@ var config = require(__libdir + '/bedrock').config;
 // identities
 config.identity.identities.push({
   type: 'Identity',
+  sysSlug: 'bedrock',
+  label: 'Bedrock',
+  email: 'bedrock@bedrock.dev',
+  sysPassword: 'password',
+  sysResourceRole: [{
+    sysRole: 'identity.registered',
+    generateResource: 'id'
+  }]
+});
+config.identity.identities.push({
+  type: 'Identity',
   sysSlug: 'dev',
   label: 'Dev',
   email: 'dev@bedrock.dev',
@@ -12,3 +23,7 @@ config.identity.identities.push({
     generateResource: 'id'
   }]
 });
+
+// identity service
+config.identity.owner =
+  config.server.baseUri + config.identity.basePath + '/bedrock';
