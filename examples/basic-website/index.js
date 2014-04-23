79f014cd591f1a7d6b7e690a0b5528a1c7b91a10
Manu Sporny <msporny@digitalbazaar.com>
2014-04-23 16:59:19 -0400

    Add example Bedrock website.

---
+++ examples/basic-website/index.js
@@ -0,0 +1,16 @@
+/*
+ * Bedrock-based Windows GUI Example
+ *
+ * Copyright (c) 2014 Digital Bazaar, Inc. All rights reserved.
+ */
+__libdir = require('path').resolve(__dirname, 'node_modules/bedrock/lib/');
+var br = require(__libdir + '/bedrock');
+// load example config
+require('./configs/example');
+
+if(module.parent) {
+  module.exports = br;
+}
+else {
+  br.start();
+}
