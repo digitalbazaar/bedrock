d50e4ce0b612866216840c0d7fe8b14cadc978f4
Manu Sporny <msporny@digitalbazaar.com>
2014-04-29 17:48:42 -0400

    Minor fix to components module.

--- examples/basic-website/site/static/app/components/components.js
+++ examples/basic-website/site/static/app/components/components.js
@@ -1,7 +1,7 @@
 /*!
  * Components module.
  *
- * Copyright (c) 2014 Accreditrust, LLC. All rights reserved.
+ * Copyright (c) 2014 Digital Bazaar, Inc. All rights reserved.
  *
  * @author Dave Longley
  */
