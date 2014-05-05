ab1c090e21f7d8be4da564fa8cce0aef21963da6
Dave Longley <dlongley@digitalbazaar.com>
2014-05-05 16:43:18 -0400

    Make helper inherit from EventEmitter to allow for extensibility.

--- tests/frontend/helper.js
+++ tests/frontend/helper.js
@@ -1,29 +1,39 @@
 /*
  * Copyright (c) 2012-2014 Digital Bazaar, Inc. All rights reserved.
  */
-var api = {};
-module.exports = api;
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
 var browser;
 var protractor;
 var by;
 var element;
 
-// called by onPrepare in config script
-api.init = function(options) {
-  api.browser = options.browser;
-  api.baseUrl = options.browser.baseUrl;
-  api.protractor = options.protractor;
-  api.element = options.element;
+function Helper() {
+  EventEmitter.call(this);
+}
+util.inherits(Helper, EventEmitter);
 
-  browser = api.browser;
-  protractor = api.protractor;
+var api = new Helper();
+module.exports = api;
+
+// called by onPrepare in config script
+Helper.prototype.init = function(options) {
+  this.browser = options.browser;
+  this.baseUrl = options.browser.baseUrl;
+  this.protractor = options.protractor;
+  this.element = options.element;
+
+  browser = this.browser;
+  protractor = this.protractor;
   by = protractor.By;
-  element = api.element;
+  element = this.element;
+
+  this.emit('init');
 };
 
 // gets a URL that returns an AngularJS page and waits for it to bootstrap
-api.get = function(url) {
+Helper.prototype.get = function(url) {
   // wait for ng-app to appear
   browser.driver.get(browser.baseUrl + url);
   browser.driver.wait(function() {
@@ -33,8 +43,8 @@ api.get = function(url) {
 };
 
 // logs in via the navbar
-api.login = function(identifier, password) {
-  api.get('/');
+Helper.prototype.login = function(identifier, password) {
+  this.get('/');
   element.all(by.model('sysIdentifier')).each(function(e) {
     e.getTagName().then(function(tagName) {
       if(tagName === 'input') {
@@ -47,16 +57,16 @@ api.login = function(identifier, password) {
 };
 
 // logs out via navbar
-api.logout = function() {
-  api.get('/');
+Helper.prototype.logout = function() {
+  this.get('/');
   element(by.binding('model.session.identity.label')).click();
   element(by.linkText('Sign Out')).click();
-  api.get('/');
+  this.get('/');
 };
 
 // runs a script in the browser's context
 // pass fn($injector) for a sync script, fn($injector, callback) for async
-api.run = function(fn) {
+Helper.prototype.run = function(fn) {
   fn = fn.toString();
   var isAsync = (fn.split('\n')[0].indexOf(',') !== -1);
   var execute = isAsync ? browser.executeAsyncScript : browser.executeScript;
