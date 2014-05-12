1bd594445f239107735de57ea3dc03808ca8cc31
Dave Longley <dlongley@digitalbazaar.com>
2014-05-12 17:31:20 -0400

    Remove unused vars.

--- tests/frontend/selectors/identity.js
+++ tests/frontend/selectors/identity.js
@@ -3,9 +3,6 @@ var helper = require('../helper.js');
 var api = {};
 module.exports = api;
 
-var expect = GLOBAL.expect;
-var ptor = GLOBAL.protractor.getInstance();
-
 api.select = function(id) {
   // TODO: implement
   return api;
