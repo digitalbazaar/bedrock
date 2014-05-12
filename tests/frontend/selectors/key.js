50acad3789406ebdbe38fadaaea1e4d5a6d220dc
Dave Longley <dlongley@digitalbazaar.com>
2014-05-12 17:56:11 -0400

    Create generic selector.

--- tests/frontend/selectors/key.js
+++ tests/frontend/selectors/key.js
@@ -1,65 +1,16 @@
 var helper = require('../helper.js');
+var selector = require('./selector.js');
 
-var api = {};
+var by = GLOBAL.by;
+var element = GLOBAL.element;
+
+var api = selector.create({add: add});
 module.exports = api;
 
-// TODO: much of the selector code could be potentially abstracted and reused
-api.create = function(options) {
-  var el = options.element.element(by.attribute('selector'));
-  var change = el.element(by.partialButtonText('Change'));
-  var modal;
-  change.isDisplayed().then(function(shown) {
-    if(shown) {
-      // change modal
-      change.click();
-      modal = element(by.modal());
-      modal.element(by.partialButtonText('Add')).click();
-
-      // add modal
-      modal = element(by.modal());
-      modal.element(by.partialButtonText('Generate Key')).click();
-      var save = modal.element(by.partialButtonText('Save'));
-      helper.waitForElement(save);
-      save.click();
-
-      // back to change modal
-      modal = element(by.modal());
-      modal.element.all(by.repeater('selected in')).last().click();
-      GLOBAL.browser.sleep(2000);
-    } else {
-      // add modal
-      el.element(by.partialButtonText('Add')).click();
-      modal = element(by.modal());
-      modal.element(by.partialButtonText('Generate Key')).click();
-      var save = modal.element(by.partialButtonText('Save'));
-      helper.waitForElement(save);
-      save.click();
-    }
-  });
-  return api;
-};
-
-api.select = function(options) {
-  var el = options.element.element(by.attribute('selector'));
-  el.evaluate('selected').then(function(selected) {
-    if(selected && selected.id === options.key) {
-      return;
-    }
-
-    el.element(by.partialButtonText('Change')).click();
-    var modal = element(by.modal());
-    var toSelect = null;
-    modal.element.all(by.repeater('selected in')).then(function(rows) {
-      rows.forEach(function(row) {
-        row.evaluate('selected').then(function(selected) {
-          if(selected.id === options.key) {
-            toSelect = row;
-          }
-        });
-      });
-    }).then(function() {
-      toSelect.click();
-    });
-  });
-  return api;
+function add(options) {
+  var modal = element(by.modal());
+  modal.element(by.partialButtonText('Generate Key')).click();
+  var save = modal.element(by.partialButtonText('Save'));
+  helper.waitForElement(save);
+  save.click();
 };
---
+++ tests/frontend/selectors/selector.js
@@ -0,0 +1,71 @@
+var by = GLOBAL.by;
+var element = GLOBAL.element;
+
+var api = {};
+module.exports = api;
+
+/**
+ * Creates a selector w/customizable behavior.
+ *
+ * @param options the options to use.
+ *          add(options) called when the add modal is open.
+ *
+ * @return the selector.
+ */
+api.create = function(options) {
+  var selector = {};
+  var add = options.add;
+
+  selector.create = function(options) {
+    var el = options.element.element(by.attribute('selector'));
+    var change = el.element(by.partialButtonText('Change'));
+    var modal;
+    change.isDisplayed().then(function(shown) {
+      if(shown) {
+        // change modal
+        change.click();
+        modal = element(by.modal());
+        modal.element(by.partialButtonText('Add')).click();
+
+        // add modal
+        add(options);
+
+        // back to change modal
+        modal = element(by.modal());
+        modal.element.all(by.repeater('selected in')).last().click();
+      } else {
+        // add modal
+        el.element(by.partialButtonText('Add')).click();
+        add(options);
+      }
+    });
+    return selector;
+  };
+
+  selector.select = function(options) {
+    var el = options.element.element(by.attribute('selector'));
+    el.evaluate('selected').then(function(selected) {
+      if(selected && selected.id === options.id) {
+        return;
+      }
+
+      el.element(by.partialButtonText('Change')).click();
+      var modal = element(by.modal());
+      var toSelect = null;
+      modal.element.all(by.repeater('selected in')).then(function(rows) {
+        rows.forEach(function(row) {
+          row.evaluate('selected').then(function(selected) {
+            if(selected.id === options.id) {
+              toSelect = row;
+            }
+          });
+        });
+      }).then(function() {
+        toSelect.click();
+      });
+    });
+    return selector;
+  };
+
+  return selector;
+};
