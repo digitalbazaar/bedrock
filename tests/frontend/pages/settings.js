2094040e7abd213b1c29712ed5ddc5527a3a35bd
Dave Longley <dlongley@digitalbazaar.com>
2014-05-14 10:54:15 -0400

    Add key revocation test.

--- tests/frontend/pages/settings.js
+++ tests/frontend/pages/settings.js
@@ -15,7 +15,8 @@ api.get = function(slug) {
   return api;
 };
 
-api.generateKey = function() {
+api.generateKey = function(options) {
+  options = options || {};
   element(by.linkText('Keys')).click();
   element(by.popover('model.showKeysMenu')).click();
   element(by.linkText('Generate Key Pair')).click();
@@ -23,5 +24,51 @@ api.generateKey = function() {
   modal.element(by.partialButtonText('Generate Key')).click();
   var save = modal.element(by.partialButtonText('Save'));
   helper.waitForElement(save);
+  if(options.label) {
+    modal.element(by.model('model.key.label')).clear();
+    modal.element(by.model('model.key.label')).sendKeys(options.label);
+  }
   save.click();
+};
+
+api.revokeKey = function(options) {
+  element(by.linkText('Keys')).click();
+  api.getKeys().then(function(keys) {
+    // support look up by id or label
+    var i;
+    var key;
+    var prop = options.id ? 'id' : 'label';
+    for(i = 0; i < keys.length; ++i) {
+      if(keys[i][prop] === options[prop]) {
+        key = keys[i];
+        break;
+      }
+    }
+    expect(key).to.exist;
+    var row = element(by.repeater('key in keys').row(i));
+    row.element(by.popover('model.showKeysActionMenu_' + i)).click();
+    element(by.linkText('Revoke')).click();
+    var modal = element(by.modal());
+    modal.element(by.partialButtonText('Revoke')).click();
+    return api.getKey('id', key.id);
+  }).then(function(key) {
+    expect(key).to.exist;
+    expect(key.revoked).to.exist;
+  });
+};
+
+api.getKeys = function() {
+  return element(by.controller('KeysCtrl')).evaluate('keys');
+};
+
+api.getKey = function(property, value) {
+  return api.getKeys().then(function(keys) {
+    for(var i = 0; i < keys.length; ++i) {
+      var key = keys[i];
+      if(key[property] === value) {
+        return key;
+      }
+    }
+    return null;
+  });
 };
