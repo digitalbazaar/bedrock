604decb0e8aef7c896ec8c77762e3509b5af0361
Dave Longley <dlongley@digitalbazaar.com>
2014-06-04 12:42:10 -0400

    Fix opencred-verifier path.

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,7 @@ module.exports = function(grunt) {
           'almond': '../../node_modules/almond/almond',
           'forge': '../../node_modules/node-forge/js',
           'jsonld': '../../node_modules/jsonld/js/jsonld',
-          'opencred-verifier': 'opencred-verifier/credentialVerifier',
+          'opencred-verifier': '../../node_modules/opencred-verifier/lib/credentialVerifier',
           'promise': '../../node_modules/es6-promise/dist/promise-0.1.1',
           'underscore': '../../node_modules/underscore/underscore',
           // override templates
