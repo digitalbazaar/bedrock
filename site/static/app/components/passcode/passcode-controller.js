0f4ae6bc7f0aa83ad9c0d5642f7297d3027fe7a7
Dave Longley <dlongley@digitalbazaar.com>
2014-08-07 13:16:49 -0400

    Use AlertService instead of feedback directive.

--- site/static/app/components/passcode/passcode-controller.js
+++ site/static/app/components/passcode/passcode-controller.js
@@ -11,58 +11,47 @@ define([], function() {
 'use strict';
 
 /* @ngInject */
-function factory($scope, IdentityService, config) {
+function factory($scope, AlertService, IdentityService, config) {
   var self = this;
   self.email = config.data.session ? config.data.session.identity.email : '';
   self.sysPasscode = config.data.sysPasscode || '';
   self.sysPasswordNew = '';
-  self.feedback = {
-    email: {},
-    password: {}
-  };
-  self.emailFeedbackTarget = $('#emailFeedbackTarget');
-  self.passwordFeedbackTarget = $('#passwordFeedbackTarget');
 
   self.sendReset = function() {
     // request a passcode
-    resetFeedback();
+    AlertService.clearFeedback();
     IdentityService.sendPasscode({
       sysIdentifier: self.email,
       usage: 'reset'
     }).then(function() {
-      self.feedback.email.success = {
+      AlertService.add('success', {
         message:
           'An email has been sent to you with password reset instructions.'
-      };
+      });
       $scope.$apply();
     }).catch(function(err) {
-      self.feedback.email.error = err;
+      AlertService.add('error', err);
       $scope.$apply();
     });
   };
 
   self.updatePassword = function() {
     // request a password reset using the given passcode
-    resetFeedback();
+    AlertService.clearFeedback();
     IdentityService.updatePassword({
       sysIdentifier: self.email,
       sysPasscode: self.sysPasscode,
       sysPasswordNew: self.sysPasswordNew
     }).then(function() {
-      self.feedback.password.success = {
+      AlertService.add('success', {
         message: 'Your password has been updated successfully.'
-      };
+      });
       $scope.$apply();
     }).catch(function(err) {
-      self.feedback.password.error = err;
+      AlertService.add('error', err);
       $scope.$apply();
     });
   };
-
-  function resetFeedback() {
-    self.feedback.email = {};
-    self.feedback.password = {};
-  }
 }
 
 return {PasscodeController: factory};
