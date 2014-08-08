3cc14f13638cca690615bea07e472a5772321aa8
Dave Longley <dlongley@digitalbazaar.com>
2014-08-08 17:30:09 -0400

    Add tools and script for compiling bootstrap.

--- Gruntfile.js
+++ Gruntfile.js
@@ -163,6 +163,22 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.config('less', {
+    compileBootstrap: {
+      options: {
+        strictMath: true,
+        sourceMap: false,
+        outputSourceFiles: false
+      },
+      files: {
+        'site/static/bootstrap/css/bootstrap.css': 'less/bootstrap.less'
+      }
+    }
+  });
+
+  grunt.registerTask('compile-bootstrap', ['less:compileBootstrap']);
+
   // default tasks
   grunt.registerTask('default', ['ngtemplates', 'cssmin', 'requirejs']);
 };
