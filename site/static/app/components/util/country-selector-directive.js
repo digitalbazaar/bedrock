af39f69c537e70e644afb184d9919555bb91d1c4
Dave Longley <dlongley@digitalbazaar.com>
2014-08-14 14:54:03 -0400

    Add help, more options etc. to br-country-selector.

--- site/static/app/components/util/country-selector-directive.js
+++ site/static/app/components/util/country-selector-directive.js
@@ -13,44 +13,71 @@ define([], function() {
 function factory(config) {
   return {
     restrict: 'E',
-    scope: {},
-    replace: true,
-    require: 'ngModel',
-    link: Link,
+    scope: {
+      model: '=brModel'
+    },
+    transclude: true,
     template: '\
-      <div> \
-      <ui-select ng-model="model.country" theme="bootstrap"> \
-        <ui-select-match placeholder="Pick a country...">{{$select.selected.name}}</ui-select-match> \
-        <ui-select-choices repeat="country in model.countries | filter: $select.search"> \
-          <div ng-bind-html="country.name | highlight: $select.search"></div> \
-        </ui-select-choices> \
-      </ui-select> \
-      </div>'
+      <div class="form-group" data-binding="{{options.name}}"> \
+        <label class="{{options.columns.label}} control-label" \
+          for="{{options.name}}">{{options.label}}</label> \
+        <div class="{{options.columns.input}} input-group"> \
+          <span ng-if="options.icon" \
+            class="input-group-addon"><i \
+            class="fa {{options.icon}}"></i></span> \
+          <span ng-if="options.image" \
+            class="input-group-addon"><img \
+            ng-src="{{options.image}}"></img></span> \
+          <ui-select ng-model="country.selected" data-track-state="help" \
+            theme="bootstrap" ng-disabled="options.disabled"> \
+            <ui-select-match placeholder="{{options.placeholder}}">{{$select.selected.name}}</ui-select-match> \
+            <ui-select-choices repeat="c in countries | filter: $select.search"> \
+              <div ng-bind-html="c.name | highlight: $select.search"></div> \
+            </ui-select-choices> \
+          </ui-select> \
+          <span class="input-group-btn"> \
+            <button type="button" class="btn btn-default" \
+              data-help-toggle="help"> \
+              <i class="fa fa-question-circle"></i> \
+            </button> \
+          </span> \
+        </div> \
+        <div ng-show="help.show" \
+          class="{{options.columns.help}} help-block br-fadein br-fadeout"> \
+          <div ng-transclude></div> \
+        </div> \
+      </div>',
+    link: Link
   };
 
-  function Link(scope, element, attrs, ngModel) {
-    var model = scope.model = {};
-    model.countries = config.constants.countries;
+  function Link(scope, element, attrs) {
+    scope.countries = config.constants.countries;
+    var country = scope.country = {selected: undefined};
+    var options = scope.options = {};
 
-    var options = {};
     scope.$watch(attrs.brOptions, function(value) {
-      if(value) {
-        options = value;
-      }
+      options = scope.options = value || {};
+      options.label = options.label || 'Country';
+      options.placeholder = options.placeholder || 'Pick a country...';
+
+      var columns = options.columns = options.columns || {};
+      columns.label = columns.label || 'col-sm-3';
+      columns.input = columns.input || 'col-sm-8';
+      columns.help = columns.help || 'col-sm-offset-3 col-sm-8';
     }, true);
 
-    // when rendering view value (country code), find the associated country
-    ngModel.$render = function() {
-      if(!ngModel.$viewValue) {
-        model.country = undefined;
+    // when external model changes, update selection
+    scope.$watch('model', function(value) {
+      if(!value) {
+        country.selected = undefined;
         return;
       }
 
-      if(options.mode === 'code' || typeof ngModel.$viewValue === 'string') {
+      if(options.mode === 'code' || typeof value === 'string') {
         // find country with matching country code
-        for(var i = 0; i < model.countries.length; ++i) {
-          if(model.countries[i].code === ngModel.$viewValue) {
-            model.country = model.countries[i];
+        for(var i = 0; i < scope.countries.length; ++i) {
+          if(scope.countries[i].code === value) {
+            country.selected = scope.countries[i];
             return;
           }
         }
@@ -59,25 +86,23 @@ function factory(config) {
 
       // by default no difference between ngModel model value and view value,
       // update model.country to render it
-      model.country = ngModel.$viewValue;
-    };
+      country.selected = value;
+    }, true);
 
-    // when country selection changes, update ngModel view
-    scope.$watch(function() {
-      return model.country;
-    }, function(country) {
+    // when country selection changes, update external model
+    scope.$watch('country.selected', function(country) {
       if(!country) {
-        ngModel.$setViewValue(undefined);
+        scope.model = undefined;
         return;
       }
 
-      if(options.mode === 'code' || typeof ngModel.$viewValue === 'string') {
-        ngModel.$setViewValue(country.code);
+      if(options.mode === 'code' || typeof scope.model === 'string') {
+        scope.model = country.code;
         return;
       }
 
       // default mode uses full country object
-      ngModel.$setViewValue(country);
+      scope.model = country;
     }, true);
   }
 }
