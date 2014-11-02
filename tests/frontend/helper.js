3741fc9c40a54e9667771d317e5100552ae0b58b
Dave Longley <dlongley@digitalbazaar.com>
2014-11-02 17:05:46 -0500

    Fix brModel and modalFooter locators; add brSelect locators.

--- tests/frontend/helper.js
+++ tests/frontend/helper.js
@@ -242,7 +242,7 @@ api.on('init', function() {
   // locate elements by br-model
   by.addLocator('brModel', function(value, parent) {
     var using = parent || document;
-    var query = "[br-model='" + value + "'] [ng-model='model']";
+    var query = "[br-model='" + value + "'] [ng-model]";
     return using.querySelectorAll(query);
   });
 
@@ -290,7 +290,7 @@ api.on('init', function() {
 
   // locate the top-level modal footer
   by.addLocator('modalFooter', function() {
-    return document.querySelectorAll('.modal > .modal-footer');
+    return document.querySelectorAll('.modal .modal-footer');
   });
 
   // locate menu items with the given text
@@ -311,5 +311,19 @@ api.on('init', function() {
     return Array.prototype.filter.call(options, function(option) {
       return (option.textContent.trim().toLowerCase() === label);
     });
+  });
+
+  // locate selector button by br-model
+  by.addLocator('brSelect', function(value, parent) {
+    var using = parent || document;
+    var query = "[br-model='" + value + "'] .ui-select-match";
+    return using.querySelectorAll(query);
+  });
+
+  // locate selector input by br-model
+  by.addLocator('brSelectInput', function(value, parent) {
+    var using = parent || document;
+    var query = "[br-model='" + value + "'] .ui-select-search";
+    return using.querySelectorAll(query);
   });
 });
