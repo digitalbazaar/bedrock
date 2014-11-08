992f1df9f85c6ed4aff9de6eba47d201f25e6fb9
Dave Longley <dlongley@digitalbazaar.com>
2014-11-08 01:24:48 -0500

    Use new angular 1.3.2 API to implement lazy compilation.

--- site/static/app/components/util/lazy-compile-directive.js
+++ site/static/app/components/util/lazy-compile-directive.js
@@ -55,74 +55,11 @@ function factory($compile, $templateCache) {
         element.removeAttr('br-lazy-id');
 
         // recompile element and link
-
-        // FIXME: remove when AngularJS 1.3.1 API is ready
-        if(!transcludeFn) {
-          $compile(element)(scope);
-          return;
-        }
-        transcludeFn = fixTranscludeScope(transcludeFn);
-        $compile(element, transcludeFn)(scope);
-
-        // FIXME: use when AngularJS 1.3.1 API is ready
-        /* $compile(el)(scope, undefined, {
+        $compile(element)(scope, undefined, {
           parentBoundTranscludeFn: transcludeFn
-        });*/
+        });
       }
     };
-  }
-
-  function fixTranscludeScope(transcludeFn) {
-    // perform mock transclusion to get default parent scope for
-    // transcluded elements (it seems angular doesn't know what this is
-    // or uses the wrong scope when you pass transcludeFn to $compile but
-    // gets it right when you call transcludeFn directly, so we do a hack
-    // to grab it and then rewrite transcludeFn)
-    try {
-      transcludeFn(function(clone, newScope) {
-        // rewrite transcludeFn to use proper scope
-        transcludeFn = fixTranscludeFn(transcludeFn, clone, newScope);
-      });
-    } catch(e) {
-      // exceptions may be thrown on the mock transclusion because
-      // required transcluded controllers are missing; however this
-      // doesn't effect our use of it to obtain the proper scope for
-      // compilation, so ignore
-    }
-    return transcludeFn;
-  }
-
-  function fixTranscludeFn(transcludeFn, clone, newScope) {
-    // since angular 1.3-rc4+ $parent isn't necessarily the scope that
-    // inherited from for non-isolate scopes (rather it is the
-    // scope of the "containing element") so use getPrototypeOf here instead
-    var parentScope = Object.getPrototypeOf(newScope);
-
-    // ensure unused scope and clone are destroyed and collected
-    newScope.$destroy();
-    clone.remove();
-    clone = newScope = null;
-
-    return function(scope, cloneAttachFn, futureParentElement) {
-      // argument adjustment
-      if(!isScope(scope)) {
-        futureParentElement = cloneAttachFn;
-        cloneAttachFn = scope;
-      }
-
-      // save containing scope and then destroy unused scope
-      var containingScope = scope.$parent;
-      scope.$destroy();
-
-      // create new child scope for clone
-      scope = parentScope.$new(false, containingScope);
-      scope.$$transcluded = true;
-      return transcludeFn(scope, cloneAttachFn, futureParentElement);
-    };
-  }
-
-  function isScope(obj) {
-    return obj && obj.$evalAsync && obj.$watch;
   }
 }
 
