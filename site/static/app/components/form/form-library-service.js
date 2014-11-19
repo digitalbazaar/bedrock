3a190eeb394f22bf7c66a9ad9aa7f38c0a92a7f1
David I. Lehn <dlehn@digitalbazaar.com>
2014-11-19 16:38:41 -0500

    Add schema.org to compact context.

--- site/static/app/components/form/form-library-service.js
+++ site/static/app/components/form/form-library-service.js
@@ -25,8 +25,9 @@ function factory(
     '@context': {
       id: '@id',
       type: '@type',
-      rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
       rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+      rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+      schema: "http://schema.org/",
       xsd: 'http://www.w3.org/2001/XMLSchema#',
       br: 'urn:bedrock:',
       layout: {'@id': 'br:layout', '@container': '@list'},
