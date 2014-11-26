fbf381fde4b9afda0219fe111582878db71aaa3c
Dave Longley <dlongley@digitalbazaar.com>
2014-11-26 17:25:53 -0500

    Support providing range options in property groups.

--- site/static/app/components/form/form-field-directive.js
+++ site/static/app/components/form/form-field-directive.js
@@ -22,8 +22,7 @@ function factory() {
     link: function(scope, element, attrs) {
       attrs.brOptions = attrs.brOptions || {};
       attrs.$observe('brOptions', function(value) {
-        var options = scope.options = scope.$eval(value) || {};
-        // TODO: grab vocab via identifier from options
+        scope.options = scope.$eval(value) || {};
       });
 
       scope.propertyId = scope.property.property.id;
@@ -31,6 +30,46 @@ function factory() {
       scope.range = scope.schema.range;
       scope.value = scope.model;
       scope.key = scope.propertyId;
+
+      // build range options, if given
+      if(scope.property.rangeOption) {
+        scope.rangeOptions = [];
+        for(var i = 0; i < scope.property.rangeOption.length; ++i) {
+          var opt = scope.property.rangeOption[i];
+          var option = {};
+          scope.rangeOptions.push(option);
+
+          // identify option by id or type
+          if(opt.rangeOptionId) {
+            option.id = opt.rangeOptionId;
+          } else if(opt.rangeOptionType) {
+            option.id = opt.rangeOptionType;
+          }
+
+          if('label' in opt) {
+            option.label = opt.label;
+          } else {
+            option.label = option.id;
+          }
+
+          // shallow copy property groups/dereference them
+          if(opt.propertyGroup) {
+            option.propertyGroup = [];
+            for(var pi = 0; pi < opt.propertyGroup.length; ++pi) {
+              // TODO: populate property groups if not embedded due to
+              // circular reference (requires access to library, so
+              // a br-library option will have to be propagated from
+              // br-form through br-form-field)
+              var group = opt.propertyGroup[i];
+              if(angular.isString(group)) {
+                option.propertyGroup.push(scope.library.groups[group]);
+              } else {
+                option.propertyGroup.push(group);
+              }
+            }
+          }
+        }
+      }
 
       // setup target for value storage
       if(scope.propertyId in scope.model) {
