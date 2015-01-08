c7cf74532da92686c6442cc631bad010f222e52a
David I. Lehn <dlehn@digitalbazaar.com>
2015-01-07 20:19:59 -0500

    Handle missing value.

--- site/static/app/components/util/ellipsis-filter.js
+++ site/static/app/components/util/ellipsis-filter.js
@@ -14,7 +14,7 @@ function factory() {
   return function(value, length) {
     length = Math.max(3, length);
     length -= 3;
-    if(value.length > length) {
+    if(value && value.length > length) {
       value = value.substr(0, length) + '...';
     }
     return value;
