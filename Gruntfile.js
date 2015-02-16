362f8186131ee2dde09fc4df0117062cb963db7f
Dave Longley <dlongley@digitalbazaar.com>
2015-02-16 15:34:32 -0500

    Remove unused grunt tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 /*
  * Bedrock Gruntfile.
  *
- * Copyright (c) 2013-2014 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2013-2015 Digital Bazaar, Inc. All rights reserved.
  */
 module.exports = function(grunt) {
   'use strict';
@@ -15,143 +15,16 @@ module.exports = function(grunt) {
   // check for ci mode
   grunt.config('ci', grunt.option('mode') === 'ci');
 
-  // optimization flag (any require.js mode, ie, 'uglify', 'none', etc
-  grunt.config('optimize',
-    grunt.option('optimize') || process.env.GRUNT_OPTIMIZE || 'uglify2');
-
-  // setup config vars for templating
-  grunt.config('dirs', {
-    'bedrock': __dirname,
-    // main project dir, override for subprojects
-    'main': __dirname
-  });
-
   // read package configuration
   grunt.config('pkg', grunt.file.readJSON('package.json'));
-
-  // cssmin
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.config('cssmin', {
-    combine: {
-      options: {
-        root: 'site/static/',
-        report: 'min'
-      },
-      files: {
-        'site/static/css/bundle.min.css': [
-          'site/static/css/app.css',
-          'site/static/css/custom.css'
-        ]
-      }
-    }
-  });
-
-  // angular templates
-  grunt.loadNpmTasks('grunt-angular-templates');
-  grunt.config('ngtemplates', {
-    myapp: {
-      options: {
-        // the module the templates will be added to
-        module: 'app.templates',
-        htmlmin: {
-          collapseBooleanAttributes:      false,
-          collapseWhitespace:             true,
-          removeAttributeQuotes:          false,
-          removeComments:                 true,
-          removeEmptyAttributes:          false,
-          removeEmptyElements:            false,
-          removeRedundantAttributes:      false,
-          removeScriptTypeAttributes:     false,
-          removeStyleLinkTypeAttributes:  false,
-          removeOptionalTags:             false
-        },
-        bootstrap: function(module, script) {
-          return [
-            "define(['angular'], function(angular) {\n",
-            "angular.module('" + module + "', [])",
-            ".run(['$templateCache', function($templateCache) {\n",
-            script,
-            '}]);\n});\n'].join('');
-        },
-        url: function(file) {
-          var idx = file.indexOf('site/static');
-          file = file.substr(idx + 'site/static'.length);
-          return file;
-        }
-      },
-      src: ['<%= dirs.bedrock %>/site/static/app/components/**/*.html'],
-      dest: 'site/static/app/templates.min.js'
-    }
-  });
-
-  // requirejs
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
-
-  // auto-generate local requirejs paths from main config
-  var requirejsMain = require('./site/static/app/main.js');
-  var requirejsPaths = {};
-  for(var key in requirejsMain.paths) {
-    var path = requirejsMain.paths[key];
-    requirejsPaths[key] = path.replace(
-      /^bower-components/g, '<%= dirs.bedrock %>/bower_components');
-  }
-
-  // overrides
-  requirejsPaths.almond = '<%= dirs.bedrock %>/bower_components/almond/almond';
-  requirejsPaths.iso8601 = '<%= dirs.bedrock %>/lib/iso8601/iso8601';
-  requirejsPaths['app/templates'] = 'app/templates.min';
-
-  grunt.config('requirejs', {
-    compile: {
-      options: {
-        baseUrl: '<%= dirs.bedrock %>/site/static',
-        paths: requirejsPaths,
-        mainConfigFile: '<%= dirs.bedrock %>/site/static/app/main.js',
-        name: 'almond',
-        include: ['app/main'],
-        insertRequire: ['app/main'],
-        out: '<%= dirs.main %>/site/static/app/main.min.js',
-        wrap: true,
-        preserveLicenseComments: false,
-        optimize: grunt.config('optimize'),
-        uglify2: {
-          compress: {
-            unused: false
-          }
-        },
-        onBuildRead: function(moduleName, path, contents) {
-          if(path.indexOf('site/static/app') === -1) {
-            return contents;
-          }
-          var ngAnnotate = require('ng-annotate');
-          var result = ngAnnotate(contents, {
-            add: true,
-            single_quotes: true
-          });
-          if(result.errors) {
-            console.log('ng-annotate failed for ' +
-              'moduleName="' + moduleName + '", path="' + path + '", ' +
-              'errors=', result.errors);
-            process.exit();
-          }
-          return result.src;
-        }
-      }
-    }
-  });
 
   var _js = [
     '*.js',
     'bin/*.js',
     'bin/**/*.js',
     'configs/*.js',
-    'email-templates/*.js',
     'lib/*.js',
     'lib/**/*.js',
-    'schemas/*.js',
-    'site/static/app/*.js',
-    'site/static/app/**/*.js',
-    '!site/static/app/**/*.min.js'//,
     //'tests/*.js',
     //'tests/**/*.js'
   ];
@@ -170,9 +43,7 @@ module.exports = function(grunt) {
 
   var _jscsOptions = {
     config: '<%= dirs.bedrock %>/.jscsrc',
-    excludeFiles: [
-      '<%= dirs.bedrock %>/lib/iso8601/*.js'
-    ]
+    excludeFiles: []
   };
   if(grunt.config('ci')) {
     _jscsOptions.reporter = 'checkstyle';
@@ -187,22 +58,6 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.config('less', {
-    compileApp: {
-      options: {
-        strictMath: true,
-        sourceMap: false,
-        outputSourceFiles: false
-      },
-      files: {
-        'site/static/css/app.css': 'less/app.less'
-      }
-    }
-  });
-
-  grunt.registerTask('compile-css', ['less:compileApp']);
-
   // default tasks
-  grunt.registerTask('default', ['ngtemplates', 'cssmin', 'requirejs']);
+  grunt.registerTask('default', []);
 };
