7dedb9be1e495cb33cc6d7e7eb5306447fbca893
Dave Longley <dlongley@digitalbazaar.com>
2022-03-21 15:55:22 -0400

    Update eslint config.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    // this is required for dynamic import()
+    ecmaVersion: 2020
+  },
   env: {
     node: true
   },
-  extends: [
-    'eslint-config-digitalbazaar'
-  ],
+  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
   ignorePatterns: ['node_modules/']
 };
