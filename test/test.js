d1b52dae2fb4c0cb059ec6ada87995f6460c6fc9
Dave Longley <dlongley@digitalbazaar.com>
2022-03-31 15:35:27 -0400

    Update imports for new package names.

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /*!
  * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
-import * as bedrock from 'bedrock';
-import 'bedrock-test';
+import * as bedrock from '@bedrock/core';
+import '@bedrock/test';
 
 bedrock.start();
