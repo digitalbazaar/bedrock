eaac0b907fcbb8c47ed70977efbeb7a781345c0a
Dave Longley <dlongley@digitalbazaar.com>
2022-04-28 18:09:20 -0400

    Use `node:` imports.

--- test/test.config.js
+++ test/test.config.js
@@ -2,8 +2,8 @@
  * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
-import {fileURLToPath} from 'url';
-import path from 'path';
+import {fileURLToPath} from 'node:url';
+import path from 'node:path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
