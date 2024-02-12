fe7fdb1837111d0e71fa77a1789407bf7805c289
Benjamin Young <byoung@digitalbazaar.com>
2024-02-12 14:30:08 -0500

    Open source under BSD-3-Clause.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 /*!
- * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 import * as bedrock from '@bedrock/core';
 import '@bedrock/test';
