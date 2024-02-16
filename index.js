8074c489c2d04dad159412ec35dbf244b2048a5f
Benjamin Young <byoung@digitalbazaar.com>
2024-02-16 10:58:48 -0500

    Fix two copyright statements.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2024 Digital Bazaar, Inc.
+ * Copyright 2012 - 2024 Digital Bazaar, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
