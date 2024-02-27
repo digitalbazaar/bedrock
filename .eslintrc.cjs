275888b155400568be96679ce9d635f3181b2ce3
Dave Longley <dlongley@digitalbazaar.com>
2024-02-27 11:21:08 -0500

    Update linting tools and fix linting errors.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,16 +15,19 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-
 module.exports = {
   root: true,
-  parserOptions: {
-    // this is required for dynamic import()
-    ecmaVersion: 2020
-  },
   env: {
     node: true
   },
-  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
-  ignorePatterns: ['node_modules/']
+  extends: [
+    'digitalbazaar',
+    'digitalbazaar/jsdoc',
+    'digitalbazaar/module'
+  ],
+  ignorePatterns: ['node_modules/'],
+  rules: {
+    'unicorn/prefer-node-protocol': 'error'
+  }
 };
+
